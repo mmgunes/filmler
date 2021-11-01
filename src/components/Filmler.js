2845be44266283276a102dc11ef67a4bb2fb0f18
mmgunes <yerisaretlerim1@gmail.com>
2021-11-01 10:39:37 +0300

    Sime metodunun Fetch ile yapılması

--- src/components/Filmler.js
+++ src/components/Filmler.js
@@ -17,7 +17,7 @@ class Filmler extends React.Component {
 
         //  filmler_db = [ yerine iki noktalı yaz. filmler_db : [  çünkü property şeklinde gelecek
         filmler_db: [],
-     
+
         aramaQuery: "",
     }
 
@@ -28,54 +28,78 @@ class Filmler extends React.Component {
     port u ayrı seç. Dosya yolunu belirt
 
     */
-  
-  
-  /*  async componentDidMount(){
-        const db_adres =  "http://localhost:3002/filmler_db";
 
-        //fetch asenkron sorgularda kullanılır. O yüzden asenkron fonksiyona çevirmek gerek
-        //asenkron olması için fetch ve json fonksiyonlarını await ve async kullan       
-        const cevap= await fetch(db_adres);
-       // console.log(cevap); 
-        const json_data= await cevap.json();
-        console.log(json_data);
-        this.setState({filmler_db : json_data});
-        console.log(this.filmler_db);
-     }
-    */
+
+    /*  async componentDidMount(){
+          const db_adres =  "http://localhost:3002/filmler_db";
+  
+          //fetch asenkron sorgularda kullanılır. O yüzden asenkron fonksiyona çevirmek gerek
+          //asenkron olması için fetch ve json fonksiyonlarını await ve async kullan       
+          const cevap= await fetch(db_adres);
+         // console.log(cevap); 
+          const json_data= await cevap.json();
+          console.log(json_data);
+          this.setState({filmler_db : json_data});
+          console.log(this.filmler_db);
+       }
+      */
 
     // Aynı İşlemi axios kütüphanesiyle yapabiliriz
 
-     async componentDidMount(){
+    async componentDidMount() {
 
-        const db_adres= await axios.get("http://localhost:3002/filmler_db");
-        console.log(db_adres);
+        const db_adres = await axios.get("http://localhost:3002/filmler_db");
+        // console.log(db_adres);
 
         //db_adres.data da json şeklinde object tutuyor yani veritabanı
-        this.setState({filmler_db: db_adres.data});
+        this.setState({ filmler_db: db_adres.data });
 
 
-     }
+    }
+
+
+    /*
+        Sil_Film = (flm) => {
+            const yeniFilmListesi = this.state.filmler_db.filter(
+                f => f.id !== flm.id,
+                //console.log("çalış2")
+            );
+    
+            //setState le yapılan işlemler react edilir.
+    
+            /* 1.yol  boş liste için uygun  
+            this.setState({
+                filmler_db : yeniFilmListesi
+            })
+            //*2.yol aynı rlimizde liste varsa
+            this.setState(state => (
+                { filmler_db: yeniFilmListesi }
+            ))
+        } */
 
 
 
-    Sil_Film = (flm) => {
+    // Fetch metoduyla API üzerinde ile silme
+
+    Sil_Film = async (flm) => {
+        const db_adres = `http://localhost:3002/filmler_db/${flm.id}`
+
+        await fetch(db_adres, {
+            //get varsayılan olduğu için method: "GET" yazmamıştık
+            method: "DELETE"
+        })
+
+
         const yeniFilmListesi = this.state.filmler_db.filter(
             f => f.id !== flm.id,
-            //console.log("çalış2")
         );
-
-        //setState le yapılan işlemler react edilir.
-
-        /* 1.yol  boş liste için uygun  
-        this.setState({
-            filmler_db : yeniFilmListesi
-        })*/
-        //*2.yol aynı rlimizde liste varsa
+        
         this.setState(state => (
             { filmler_db: yeniFilmListesi }
         ))
+
     }
+
 
     Ara_Film_Fnk = (event) => {
         //     const yaz= event.target.value
@@ -84,8 +108,8 @@ class Filmler extends React.Component {
         this.setState({ aramaQuery: event.target.value })
     }
 
-    Filtrele= () =>{
-        return  this.state.filmler_db.filter(
+    Filtrele = () => {
+        return this.state.filmler_db.filter(
             s => {
                 return s.name.toLowerCase().indexOf(this.state.aramaQuery.toLowerCase()) !== -1
             }
