78fd68fbc36272db3de7a3cd3456a69b6b45958f
mmgunes <yerisaretlerim1@gmail.com>
2021-11-02 09:46:59 +0300

    Film Ekle Class Componentini Ekledik Şablon olarak

--- src/components/Filmler.js
+++ src/components/Filmler.js
@@ -2,103 +2,32 @@ import React from "react";
 import SearchBar from "./SearchBar";
 import MovieList from "./MovieList";
 import axios from "axios";
+import FilmEkle from "./FilmEkle";
 
 
-//Functional Component olmaz çünkü dinamik gösterilecek
 
-/*const Filmler = () => {
-    return ( <h1>Filmlerim</h1>)
-}*/
-
-//Class Component render()) metodunu unutma
 class Filmler extends React.Component {
 
     state = {
 
-        //  filmler_db = [ yerine iki noktalı yaz. filmler_db : [  çünkü property şeklinde gelecek
         filmler_db: [],
 
         aramaQuery: "",
     }
 
 
-    /* json üzerinden çekmek için npm json server ekle npm i json-server
-    npx json-server --watch src/api/filmler_db.json --port 3002
-    
-    port u ayrı seç. Dosya yolunu belirt
 
-    */
-
-
-    /*  async componentDidMount(){
-          const db_adres =  "http://localhost:3002/filmler_db";
-  
-          //fetch asenkron sorgularda kullanılır. O yüzden asenkron fonksiyona çevirmek gerek
-          //asenkron olması için fetch ve json fonksiyonlarını await ve async kullan       
-          const cevap= await fetch(db_adres);
-         // console.log(cevap); 
-          const json_data= await cevap.json();
-          console.log(json_data);
-          this.setState({filmler_db : json_data});
-          console.log(this.filmler_db);
-       }
-      */
 
     // Aynı İşlemi axios kütüphanesiyle yapabiliriz
 
     async componentDidMount() {
 
         const db_adres = await axios.get("http://localhost:3002/filmler_db");
-        // console.log(db_adres);
-
         //db_adres.data da json şeklinde object tutuyor yani veritabanı
         this.setState({ filmler_db: db_adres.data });
 
 
     }
-
-
-    /*
-        Sil_Film = (flm) => {
-            const yeniFilmListesi = this.state.filmler_db.filter(
-                f => f.id !== flm.id,
-                //console.log("çalış2")
-            );
-    
-            //setState le yapılan işlemler react edilir.
-    
-            /* 1.yol  boş liste için uygun  
-            this.setState({
-                filmler_db : yeniFilmListesi
-            })
-            //*2.yol aynı rlimizde liste varsa
-            this.setState(state => (
-                { filmler_db: yeniFilmListesi }
-            ))
-        } */
-
-
-
-    // Fetch metoduyla API üzerinde ile silme
-
-   /* Sil_Film = async (flm) => {
-        const db_adres = `http://localhost:3002/filmler_db/${flm.id}`
-
-        await fetch(db_adres, {
-            //get varsayılan olduğu için method: "GET" yazmamıştık
-            method: "DELETE"
-        })
-
-
-        const yeniFilmListesi = this.state.filmler_db.filter(
-            f => f.id !== flm.id,
-        );
-
-        this.setState(state => (
-            { filmler_db: yeniFilmListesi }
-        ))
-
-    } */
 
     // AXIOS kütüphanesiyle silme
 
@@ -121,9 +50,6 @@ class Filmler extends React.Component {
 
 
     Ara_Film_Fnk = (event) => {
-        //     const yaz= event.target.value
-        //    this.setState({aramaQuery:yaz})    
-
         this.setState({ aramaQuery: event.target.value })
     }
 
@@ -137,12 +63,6 @@ class Filmler extends React.Component {
 
 
     render() {
-
-        // let filtrelen_Filmler = this.state.filmler_db.filter(
-        //     s => {
-        //         return s.name.toLowerCase().indexOf(this.state.aramaQuery.toLowerCase()) !== -1
-        //     }
-        // )
 
         return (
             <div className="container">
@@ -159,6 +79,7 @@ class Filmler extends React.Component {
                     filmler_prop={this.Filtrele()}
                     Sil_Film_Prop={this.Sil_Film}
                 />
+                <FilmEkle />
             </div>
         )
     }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -7,10 +7,10 @@ class SearchBar extends React.Component {
 
     //  state ve fonksiyonları render() in üstüne yaz
 
-  /*  state = {
-        aramaQuery: "w1",
-        ali: "ali"
-    }*/
+    /*  state = {
+          aramaQuery: "w1",
+          ali: "ali"
+      }*/
 
     //Enter a basınca normalde sayfa yenilenir. onSubmit() çalışır
     //basınca yenilemesin preventDefault()
@@ -19,30 +19,35 @@ class SearchBar extends React.Component {
         event.preventDefault();
     }
 
-       AramaFnk = (event) => {
-           const yaz= event.target.value;
-        this.setState({aramaQuery:yaz}) 
-       }
+    AramaFnk = (event) => {
+        const yaz = event.target.value;
+        this.setState({ aramaQuery: yaz })
+    }
 
- 
+
 
     render() {
         return (
             //onsubmit i çağır
             <form onSubmit={this.sayfayiYenilemesinFnk}>
-                <div className="form-row mb-5 mt-5">
-                    <div className="col-12">
-                        {/* onChange={e=>console.log("değiştim")} her değişiklikte yapması gerekeni 
-                    onChange={event=>console.log(event.target.value)} fonk ile içindeki value al */}
+           
+                <div className="form-row mb-5">
+                    <div className="col-10 mt-5">
                         <input
 
                             // onChange={event => this.setState({aramaQuery:event.target.value}) }
-                           // onChange={this.AramaFnk}
-                           onChange={this.props.filmAraProp}
+                            // onChange={this.AramaFnk}
+                            onChange={this.props.filmAraProp}
                             type="text" className="form-control"
                             placeholder="Aranacak film"
-                           // value={this.state.aramaQuery}
+                        // value={this.state.aramaQuery}
                         />
+                    </div>
+                    <div className="col-2 mt-5">
+                        <button type="button"
+                            className="btn btn-md btn-danger"
+                            >Film Ekle
+                        </button>
                     </div>
                 </div>
             </form>
