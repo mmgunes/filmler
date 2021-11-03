74b500791006536440f45a10eb43bd9105446f9b
mmgunes <yerisaretlerim1@gmail.com>
2021-11-03 11:08:59 +0300

    state ve didmountla başlangıç değerlerini çekmesi

--- src/components/FilmDuzenle.js
+++ src/components/FilmDuzenle.js
@@ -1,11 +1,40 @@
 import React from "react";
-
+import axios from "axios";
 
 class FilmDuzenle extends React.Component {
 
-    sayfaYenilenmesin = (e) =>{
+    //Başlangıç stateleri boş
+    state = {
+        name: "",
+        rating: "",
+        overview: "",
+        imageUrl: ""
+    }
+
+
+    //başlangıçta düzenlenecek filmin değerlerin gelmesi için
+    async componentDidMount() {
+
+        const film_id = this.props.match.params.id; //id yi sayfanın propsundan çekebiliriz
+        //console.log(film_id)
+        const duzenlenecek_flm = await axios.get(`http://localhost:3002/filmler_db/${film_id}`)
+        // console.log(duzenlenecek_flm.data)
+        const film_bilgi = duzenlenecek_flm.data;
+
+        this.setState(
+            {
+                name: film_bilgi.name,
+                rating: film_bilgi.rating,
+                overview: film_bilgi.overview,
+                imageUrl: film_bilgi.imageUrl
+            }
+        )
+
+    }
+
+    sayfaYenilenmesin = (e) => {
         e.preventDefault();
-       
+
     }
 
     render() {
@@ -19,14 +48,16 @@ class FilmDuzenle extends React.Component {
                             <label htmlFor="inputName">Name</label>
                             <input type="text"
                                 className="form-control"
-                                name="name" />
+                                name="name"
+                                value={this.state.name} />
                         </div>
                         <div className="form-group col-md-2">
                             <label htmlFor="inputRating">Rating</label>
                             <input
                                 type="text"
                                 className="form-control"
-                                name="rating" />
+                                name="rating"
+                                value={this.state.rating} />
                         </div>
                     </div>
                     <div className="form-row">
@@ -35,15 +66,18 @@ class FilmDuzenle extends React.Component {
                             <input
                                 type="text"
                                 className="form-control"
-                                name="imageUrl" />
-                                                    </div>
+                                name="imageUrl" 
+                                value={this.state.imageUrl}/>
+                        </div>
                     </div>
                     <div className="form-row">
                         <div className="form-group col-md-12">
                             <label htmlFor="overviewTextarea">Overview</label>
                             <textarea
                                 className="form-control"
-                                name="overview" rows="5"></textarea>
+                                name="overview" rows="5"
+                                value={this.state.overview}
+                                ></textarea>
                         </div>
                     </div>
                     <input type="submit" className="mb-5 mt-3 btn btn-primary btn-lg btn-block" value="Add Movie" />
